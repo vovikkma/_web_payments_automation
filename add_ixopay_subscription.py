e5a5dae23ab2e0cba00f9929d5253e95e28a54ab
Vovik <vovik@Vovik-2.local>
2025-04-04 12:33:29 +0300

    feat: handle plans without trial

--- add_ixopay_subscription.py
+++ add_ixopay_subscription.py
@@ -11,17 +11,22 @@ load_dotenv()
 
 api_url_dev = os.getenv("WEB_PAYMENT_SERVICE_DEV")
 api_url_prod = os.getenv("WEB_PAYMENT_SERVICE_PROD")
-plans = os.getenv("PLANS_WITH_TRIAL_VAT")
+plans = os.getenv("PLANS_WITHOUT_TRIAL")
 
 
 def form_request_body(row):
     subscription_duration = row['duration']
-    vat_rate_normalized = format(Decimal(row['vat_rate']).normalize(), 'f')
-    amplitude_id_vat_suffix = '' if float(row['vat_rate']) == 1.0 else f"_vat_{vat_rate_normalized.rstrip('0').rstrip('.') if '.' in vat_rate_normalized else vat_rate_normalized}"
-    trial_duration = '' if row['trial_duration'] == 'NULL' else row['trial_duration']
-    trial_price = '' if float(row['trial_price']) == 0 else format(float(row['trial_price']), ".2f")
-    amplitude_id = f"{subscription_duration.replace(' ', '')}_{float(row['price']):.2f}$_with_trial_{trial_duration.replace(' ', '')}_{trial_price}${amplitude_id_vat_suffix}"
-    #amplitude_id = row['amplitude_id']
+    
+    amplitude_id = row['amplitude_id']
+    
+    if amplitude_id == '':
+        vat_rate_normalized = format(Decimal(row['vat_rate']).normalize(), 'f')
+        amplitude_id_vat_suffix = '' if float(row['vat_rate']) == 1.0 else f"_vat_{vat_rate_normalized.rstrip('0').rstrip('.') if '.' in vat_rate_normalized else vat_rate_normalized}"
+        trial_duration = '' if row['trial_duration'] == 'NULL' else row['trial_duration']
+        trial_price = '' if float(row['trial_price']) == 0 else format(float(row['trial_price']), ".2f")
+        amplitude_id = f"{subscription_duration.replace(' ', '')}_{float(row['price']):.2f}$_with_trial_{trial_duration.replace(' ', '')}_{trial_price}${amplitude_id_vat_suffix}"
+    
+
     body = {
         "id": row["id"],
         "project": row['project'],
